f02a5e7453574521c52e15d494c002d4ae80014e
vanekt <vanekt@yandex.ru>
2023-11-22 14:09:06 -0300

    v1.1.0

--- src/index.js
+++ src/index.js
@@ -1,26 +1,150 @@
-import React from "react";
-import { Screen, CardScreen, QueryScreen } from "./components";
+import React, { useState, useEffect } from "react";
+import { useParams, useLocation } from "react-router-dom";
 
-// TODO implement
-function useFetch() {
-  return {
-    data: {},
-    loading: false,
-    error: null,
-    refetch: () => {},
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+function StaticScreen({ dataFn, renderFn, children, vars = {} }) {
+  const [data, setData] = useState(children[0]);
+
+  return renderFn(data, {
+    screen: {
+      refresh: async () => {
+        return setData(children[0]);
+      },
+      load: async (source, params) => {
+        return dataFn(source, params).then(setData);
+      },
+    },
+    ...vars,
+  });
+}
+
+function Screen({
+  dataFn,
+  renderFn,
+  onLoading,
+  onError,
+  source,
+  params = {},
+  deps = [],
+  vars = {},
+}) {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
+
+  const fetchData = async (s, p) => {
+    setLoading(true);
+
+    return dataFn(s, p)
+      .then((data) => {
+        setData(data);
+        setError(null);
+      })
+      .catch(setError)
+      .finally(() => {
+        setLoading(false);
+      });
   };
+
+  useEffect(() => {
+    fetchData(source, params);
+  }, [source, ...deps]);
+
+  if (loading) {
+    return typeof onLoading === "function" ? onLoading() : null;
+  }
+
+  if (error) {
+    return typeof onError === "function" ? onError(error) : "Error";
+  }
+
+  return renderFn(data, {
+    screen: {
+      refresh: async () => {
+        return fetchData(source, params);
+      },
+      load: async (source, params) => {
+        return dataFn(source, params).then(setData);
+      },
+    },
+    ...vars,
+  });
+}
+
+function CardScreen({
+  source,
+  dataFn,
+  renderFn,
+  idKey = "id",
+  vars = {},
+  ...props
+}) {
+  const { id = null } = useParams();
+
+  return (
+    <Screen
+      dataFn={dataFn}
+      renderFn={renderFn}
+      source={source}
+      params={{ [idKey]: id }}
+      deps={[id]}
+      vars={vars}
+      {...props}
+    />
+  );
+}
+
+function QueryScreen({
+  dataFn,
+  renderFn,
+  source,
+  queryMap,
+  vars = {},
+  ...props
+}) {
+  const query = useQuery();
+
+  const qqq = Object.fromEntries(
+    Object.entries(queryMap || {}).map((i) => {
+      const v = query.get(i[0]);
+      switch (i[1]) {
+        case "integer":
+          return [i[0], parseInt(v)];
+        default:
+          return [i[0], v];
+      }
+    }),
+  );
+
+  return (
+    <Screen
+      dataFn={dataFn}
+      renderFn={renderFn}
+      source={source}
+      params={qqq}
+      deps={Object.values(qqq)}
+      vars={{
+        query,
+        ...vars,
+      }}
+      {...props}
+    />
+  );
 }
 
 function initModule({ onLoading, onError, datasources: ds }) {
   const datasources = {
-    useFetch,
+    fetch: window.fetch,
     ...ds,
   };
 
   return {
     name: "screen",
     components: {
-      screen: ({ dataFn = "useFetch", ...props }, { render }) => (
+      screen: ({ dataFn = "fetch", ...props }, { render }) => (
         <Screen
           dataFn={datasources[dataFn]}
           renderFn={render}
@@ -29,7 +153,12 @@ function initModule({ onLoading, onError, datasources: ds }) {
           {...props}
         />
       ),
-      card_screen: ({ dataFn = "useFetch", ...props }, { render }) => (
+      static_screen: ({ dataFn = "fetch", ...props }, { render, children }) => (
+        <StaticScreen dataFn={datasources[dataFn]} renderFn={render} {...props}>
+          {children}
+        </StaticScreen>
+      ),
+      card_screen: ({ dataFn = "fetch", ...props }, { render }) => (
         <CardScreen
           dataFn={datasources[dataFn]}
           renderFn={render}
@@ -38,7 +167,7 @@ function initModule({ onLoading, onError, datasources: ds }) {
           {...props}
         />
       ),
-      query_screen: ({ dataFn = "useFetch", ...props }, { render }) => (
+      query_screen: ({ dataFn = "fetch", ...props }, { render }) => (
         <QueryScreen
           dataFn={datasources[dataFn]}
           renderFn={render}
